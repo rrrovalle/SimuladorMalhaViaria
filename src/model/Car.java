6bb2499d6c4179461983987ad0f986b659375197
Guilherme Deschamps <guilhermerd6@hotmail.com>
2021-02-07 10:54:30 -0300

    Parcialmente implementada a lógica de verificar cruzamentos

--- src/model/Car.java
+++ src/model/Car.java
@@ -6,6 +6,7 @@ import utils.MatrixManager;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -17,9 +18,11 @@ public class Car extends Thread {
     private boolean outOfRoad = false;
     private final FrameController frameController;
     private final MatrixManager matrixManager = MatrixManager.getInstance();
+    private boolean estaNoCruzamento = false;
 
     private Cell cell;
     private Cell nextCell = new Cell(0);
+    private Cell saidaCruzamento;
 
     public Car(FrameController frameController) {
         this.frameController = frameController;
@@ -40,15 +43,53 @@ public class Car extends Thread {
             if (checkLastCell()) {
                 System.out.println("ultima celula");
                 outOfRoad = true;
-            } else if (nextCell.isStopCell()) {
-//                verificaCruzamento();
-            } else if(!nextCell.containsCar()) {
+            } else if (nextCell.isStopCell() || estaNoCruzamento) {
+                verificaCruzamento();
+            } else if (!nextCell.containsCar()) {
                 movimenta();
 
                 frameController.resetCarCell(this);
                 cell = frameController.getCellAtPosition(row, column);
                 frameController.updateRoadView(this);
             }
+        }
+    }
+
+    //O verifica cruzamento é uma região crítica. Não pode acontecer de o carro ver o espaço vazio à sua frente,
+    // e aí perder o processador por um instante, e quando o carro for tentar se mover o espaço na verdade não está vazio.
+    private void verificaCruzamento() {
+        if(saidaCruzamento == null) {
+            List<Cell> saidasCruzamento = new ArrayList<>();
+
+            Cell cell = nextCell;
+
+            // For responsável por passar pelas 4 células do cruzamento
+            for (int i = 0; i < 4; i++) {
+                int moveType = cell.getMoveType();
+                cell = getNextCell(cell);
+
+                switch (moveType) {
+                    case 9:
+                        //adiciona saída na lista
+                        break;
+                    case 10:
+                        //adiciona saída na lista
+                        break;
+                    case 11:
+                        //adiciona saída na lista
+                        break;
+                    case 12:
+                        //adiciona saída na lista
+                        break;
+                }
+            }
+
+            estaNoCruzamento = true;
+            Collections.shuffle(saidasCruzamento);
+            saidaCruzamento = saidasCruzamento.get(0);
+        } else {
+//            movimenta();
+//            Aqui movimentaremos o carro e procuraremos pela saída quando ele já estiver dentro do cruzamento
         }
     }
 
@@ -61,25 +102,21 @@ public class Car extends Thread {
 
         switch (moveType) {
             case 1:
-                System.out.println("andou cima");
                 this.setRow(this.getRow() - 1);
                 this.setColumn(this.getColumn());
                 this.nextCell = frameController.getCellAtPosition(this.getRow() - 1, this.getColumn());
                 break;
             case 2:
-                System.out.println("andou dir");
                 this.setRow(this.getRow());
                 this.setColumn(this.getColumn() + 1);
                 this.nextCell = frameController.getCellAtPosition(this.getRow(), this.getColumn() + 1);
                 break;
             case 3:
-                System.out.println("andou baixo");
                 this.setRow(this.getRow() + 1);
                 this.setColumn(this.getColumn());
                 this.nextCell = frameController.getCellAtPosition(this.getRow() + 1, this.getColumn());
                 break;
             case 4:
-                System.out.println("andou esq");
                 this.setRow(this.getRow());
                 this.setColumn(this.getColumn() - 1);
                 this.nextCell = frameController.getCellAtPosition(this.getRow(), this.getColumn() - 1);
@@ -136,5 +173,45 @@ public class Car extends Thread {
             System.out.println("inserido em:" + row + "," + col);
             return true;
         }
+    }
+
+    // Para este método precisaremos colocar as posições nas célular. Ele retorna a próxima célula em relação a uma célula qualquer,
+    // e vai ser usado para mapearmos o cruzamento sem precisar mover o carro..
+    private Cell getNextCell(Cell cell) {
+        int moveType;
+
+        if (cell.getMoveType() > 4 && cell.getMoveType() <= 8) {
+            moveType = cell.getMoveType() - 4;
+        } else if (cell.getMoveType() > 8) {
+            moveType = cell.getMoveType() - 8;
+        } else {
+            moveType = cell.getMoveType();
+        }
+
+        switch (moveType) {
+            case 1:
+                this.setRow(this.getRow() - 1);
+                this.setColumn(this.getColumn());
+                this.nextCell = frameController.getCellAtPosition(this.getRow() - 1, this.getColumn());
+                break;
+            case 2:
+                this.setRow(this.getRow());
+                this.setColumn(this.getColumn() + 1);
+                this.nextCell = frameController.getCellAtPosition(this.getRow(), this.getColumn() + 1);
+                break;
+            case 3:
+                this.setRow(this.getRow() + 1);
+                this.setColumn(this.getColumn());
+                this.nextCell = frameController.getCellAtPosition(this.getRow() + 1, this.getColumn());
+                break;
+            case 4:
+                this.setRow(this.getRow());
+                this.setColumn(this.getColumn() - 1);
+                this.nextCell = frameController.getCellAtPosition(this.getRow(), this.getColumn() - 1);
+                break;
+            default:
+                break;
+        }
+
     }
 }
